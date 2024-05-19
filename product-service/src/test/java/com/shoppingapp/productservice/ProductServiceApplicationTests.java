d3e89b6d19bfaae7a84bf6d60a0b78a51d6b88ae
ValeriaG23 <valeria.guranda@isa.utm.md>
2024-05-19 19:33:03 +0300

    some changes

--- product-service/src/test/java/com/shoppingapp/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/shoppingapp/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,29 @@
 package com.shoppingapp.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shoppingapp.productservice.repository.ProductRepository;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Container
+	//specificam vers mongo care o utilizam
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo: 4.4.2");
+	 
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
 
+
+	}
+	
+	
 }
