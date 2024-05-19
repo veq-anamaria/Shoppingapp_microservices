37531bd76df306f99cc8821182e78344956d8a7d
ValeriaG23 <valeria.guranda@isa.utm.md>
2024-05-19 16:25:37 +0300

    change ProductService

--- product-service/src/main/java/com/shoppingapp/productservice/service/ProductService.java
+++ product-service/src/main/java/com/shoppingapp/productservice/service/ProductService.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Service;
 
 import com.shoppingapp.productservice.dto.ProductRequest;
 import com.shoppingapp.productservice.model.Product;
+import com.shoppingapp.productservice.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 //serviciu la care vom apela de fiecare data cand accesam un produs
 @Service
+@RequiredArgsConstructor //in momentul compilarii creaaza constr de care avem nevoie
+@Slf4j //pentru loguri
 public class ProductService {
+//constructor injection depozitul de produse
+    private final ProductRepository productRepository;
+
+
     //cream un produs care preia cererea de produs
     public void createProduct(ProductRequest productRequest){
         //mapam cererea de produs catre modelul de produs, deci pentru asta cream var de tip produs
@@ -17,7 +27,10 @@ public class ProductService {
         .price(productRequest.getPrice())
         .build();
 
+        //salvam produsul in baza de date
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+        //odata ce adaugam prod, vor aparea loguri
         
-
     }
 }
